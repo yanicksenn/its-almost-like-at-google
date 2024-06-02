cbe72a4a8751cc0f4d36808d0f78dcbad120d7cb
Yanick Senn <ys+github@yanicksenn.com>
2024-06-02 16:23:36 +0200

    Add convenience script

--- scripts.sh
+++ scripts.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-# Please don't have me.
+# Please don't hate me.
 alias blaze="bazel"
 
 cdws() {
 	cd /Users/yanicksenn/Documents/Projects/bazel
+}
+
+commit() {
+	message=$1
+	hg add .
+	hg commit -m "$1"
+
+	git add .
+	git commit -m "$1"
 }
