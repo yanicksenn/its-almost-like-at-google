9a9eb1170b690229db382b84dfd69481c52e62ea
Yanick Senn <ys+github@yanicksenn.com>
2024-06-07 21:46:48 +0200

    Fix compiling error

--- python/com/yanicksenn/templating/templating.py
+++ python/com/yanicksenn/templating/templating.py
@@ -1,8 +1,7 @@
 from python.com.yanicksenn.templating import templating_engine
 
 def main():
-    
-    templating_engine.run()
+    templating_engine.run("/path/to/template")
 
 if __name__ == '__main__':
     main()
