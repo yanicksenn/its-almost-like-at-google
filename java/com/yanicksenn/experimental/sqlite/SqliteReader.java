dc14c0c42f3fe7f51ce663122dbef9f2129c8967
Yanick Senn <ys+github@yanicksenn.com>
2024-07-01 23:19:00 +0200

    Add base files for sqlite reader and writer

---
+++ java/com/yanicksenn/experimental/sqlite/SqliteReader.java
@@ -0,0 +1,8 @@
+package com.yanicksenn.experimental.sqlite;
+
+public final class SqliteReader {
+  public static void main(String[] args) {
+    // TODO - yanicksenn: Write SQLite reader.
+    System.out.println("Hello, Reader!");
+  }
+}
---
+++ java/com/yanicksenn/experimental/sqlite/SqliteWriter.java
@@ -0,0 +1,8 @@
+package com.yanicksenn.experimental.sqlite;
+
+public final class SqliteWriter {
+  public static void main(String[] args) {
+    // TODO - yanicksenn: Writer SQLite writer.
+    System.out.println("Hello, Writer!");
+  }
+}
