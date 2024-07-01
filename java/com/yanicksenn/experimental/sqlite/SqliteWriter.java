08a91533e39b92b969eec9f1590ac6d30630575b
Yanick Senn <ys+github@yanicksenn.com>
2024-07-01 23:19:28 +0200

    Amend

--- java/com/yanicksenn/experimental/sqlite/SqliteWriter.java
+++ java/com/yanicksenn/experimental/sqlite/SqliteWriter.java
@@ -2,7 +2,7 @@ package com.yanicksenn.experimental.sqlite;
 
 public final class SqliteWriter {
   public static void main(String[] args) {
-    // TODO - yanicksenn: Writer SQLite writer.
+    // TODO - yanicksenn: Write SQLite writer.
     System.out.println("Hello, Writer!");
   }
 }
