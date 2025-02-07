b17c764c677cc083f1770981ad0f46f21fa381bf
Yanick Senn <ys+github@yanicksenn.com>
2025-02-07 15:59:15 +0100

    Making go build

---
+++ gotests/com/yanicksenn/taste/parser/parser_test.go
@@ -0,0 +1,12 @@
+package github.com/yanicksenn/its-almost-like-at-google/go/com/yanicksenn/taste/parser;
+
+import (
+	"testing"
+	"github.com/yanicksenn/its-almost-like-at-google/go/com/yanicksenn/taste/parser"
+	"github.com/yanicksenn/its-almost-like-at-google/go/com/yanicksenn/taste/shared"
+)
+
+func TestParse(t *testing.T) {
+	t.Fatalf("This test is actually executed")
+}
+
